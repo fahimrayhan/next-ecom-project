dfe58ac846d6b02bb28ff76848c96bd68e74f7fb
Fahim Rayhan <fahimrayhan786@gmail.com>
2022-03-19 04:22:41 +0600

    product detail page added

---
+++ pages/product/[id].jsx
@@ -0,0 +1,80 @@
+import Head from "next/head";
+import { useState } from "react";
+import {getData} from "../../utils/fetchData"
+
+const DetailProduct = (props) =>{
+
+    const [product] = useState(props.product)
+    const [tab,setTab] = useState(0)
+ 
+
+    const isActive = (key) =>{
+        if (tab===key) {
+            return " active"
+        }
+        else{
+            return ""
+        }
+    }
+
+    
+
+    return (
+        <div className="row detail_page">
+            <Head>
+                <title>Detail Product</title>
+            </Head>
+             <div className="col-md-6">
+                <img src={product.images[tab].url} alt={product.images[tab].url} className="d-block img-thumbnail rounded mt-4 w-100"
+                    style={{height:'350px'}}
+                />
+                <div className="row mx-0" style={{ cursor: 'pointer' }}>
+                    {
+                        product.images.map((img,key) =>{
+                            return(
+                                <img key={key} src={img.url} alt={img.url} className={`img-thumbnail rounded ${isActive(key)}`}
+                                    style={{height:'80px', width:'20%'}}
+                                    onClick={()=> setTab(key)}
+                                />
+                            )
+                        })
+                    }
+                </div>
+             </div>
+             <div className="col-md-6">
+                 <h2 className="text-uppercase">{product.title}</h2>
+                 <h5 className="text-uppercase">${product.price}</h5>
+
+                 <div className="row mx-0 d-flex justify-content-between">
+                    {
+                        product.inStock > 0?
+                        <h6 className="text-danger">In Stock: {product.inStock}</h6>:
+                        <h6 className="text-danger">Out Of Stock</h6>
+                    }
+                    <h6 className="text-danger">Sold: {product.sold}</h6>
+                 </div>
+                 <div className="my-2">
+                     {product.description}
+                 </div>
+                 <div className="my-2">
+                     {product.content}
+                     {product.content}
+                     {product.content}
+                 </div>
+
+                <button className="btn btn-dark d-block my-3 px-5" type="button">Buy</button>
+
+             </div>
+        </div>
+    )
+}
+
+export async function getServerSideProps({params:{id}}) {
+    
+    const res = await getData(`product/${id}`)
+    return {
+        props: {product:res.product}
+    }
+}
+
+export default DetailProduct
